7639d709330b2fcd2757e98ce8aa423449cefeef
hyerin <lyyyn@naver.com>
2022-04-25 18:13:49 +0900

    Create: 메인 페이지 큰 틀 작성

---
+++ frontend/src/views/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Login from "../components/main/Login.js";
+
+function MainPage() {
+  React.useEffect(() => {});
+  return (
+    <>
+      <Login></Login>
+    </>
+  );
+}
+
+export default MainPage;
