0a46736bb683a3841d34a99787b8cacf016bf4f5
이명주 <myungju25@naver.com>
2022-04-26 16:38:24 +0900

    Feat: Member 테이블 JPA 매핑

---
+++ backend/src/main/java/com/pillimi/backend/db/entity/Member.java
@@ -0,0 +1,57 @@
+package com.pillimi.backend.db.entity;
+
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Entity
+@Table(name = "member")
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberSeq;
+
+    @Column
+    private String memberUid;
+
+    @Column
+    private String memberNickname;
+
+    @Column
+    private String memberEmail;
+
+    @Column
+    private String memberImage;
+
+    @Column
+    private LocalDate memberBirthdate;
+
+    @Column
+    private Boolean memberGender;
+
+    @Column
+    private String memberPhone;
+
+    @Column
+    private Boolean memberIsdeleted;
+
+    @Column
+    private Boolean memberIsfirst;
+
+    @Column
+    private Boolean memberIsprotector;
+
+
+}
