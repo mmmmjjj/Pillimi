ff11dc8f0ab26254ba526d08757b0caa788a42ba
이명주 <myungju25@naver.com>
2022-04-26 16:37:48 +0900

    Create: API 공통 Base 클래스 생성

---
+++ backend/src/main/java/com/pillimi/backend/common/model/ResponseMessage.java
@@ -0,0 +1,16 @@
+package com.pillimi.backend.common.model;
+
+public class ResponseMessage {
+
+    // Common
+    public static final String SERVER_ERROR =  "서버 에러";
+    public static final String UNAUTHORIZED = "인증 되지 않은 유저";
+    public static final String FORBIDDEN = "허용하지 않는 접근";
+    public static final String NOT_FOUND = "Not Found 오류";
+
+    // Auth
+    public static final String GET_KAKAO_ACCESS_TOKEN = "카카오 토큰 생성 성공";
+
+    // Login
+    public static final String LOGIN = "로그인 성공";
+}
