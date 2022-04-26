23c237fa06e40052bfd43dfee64569b9a9b27d5a
고주희 <budludl@naver.com>
2022-04-26 17:40:45 +0900

    Feat: 복용 확인 화면 추가 #S06P31A307-98

--- frontend/src/components/Member/MemberPillCheck/PillCheckAlarm.js
+++ frontend/src/components/Member/MemberPillCheck/PillCheckAlarm.js
@@ -4,19 +4,27 @@ import React from "react";
 // reactstrap components
 import { Container } from "reactstrap";
 import MemberPillCheck from "../MemberPillCheck";
+import style from "../css/MemberPillCheck.module.css"
+import { AiOutlineCheckCircle } from 'react-icons/ai';
 
 // core components
 
 function PillCheckAlarm(props) {
   return (
     <>
-      <Container>
+      <Container className={`${style.center}`}>
         <MemberPillCheck></MemberPillCheck>
-        <div>
-          감사합니다!<br></br>
-          약이 확인되었습니다!
+        <div className={`${style.allcenter}`}>
+          <div>
+            <span className={`${style.bigfont}`}>
+              감사합니다!<br></br>
+              약이 확인되었습니다!
+            </span>
+            <div>
+              <AiOutlineCheckCircle className="mt-3" size={"2em"} color="2BC228"></AiOutlineCheckCircle>
+            </div>
+          </div>
         </div>
-        
       </Container>
     </>
   )
