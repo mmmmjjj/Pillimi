4b76f8835befb3eb1bacc07107dc18671685fe2d
hyerin <lyyyn@naver.com>
2022-04-27 23:35:23 +0900

    Style: 들어갈 내용 변경

--- frontend/src/components/pill/PillDetail.js
+++ frontend/src/components/pill/PillDetail.js
@@ -10,19 +10,25 @@ function PillDetail() {
       <br></br>
       <h3 className={PillDetailCSS.PillDetailTitle}>알약 정보</h3>
       <hr></hr>
+      <br></br>
       <Button className={PillDetailCSS.AddBtn}>추가</Button>
       <br></br>
       <br></br>
-      <h3 style={{ textAlign: "center" }}>이미지</h3>
+      <h3 style={{ textAlign: "center" }}>
+        <img
+          alt="pillImg"
+          src="http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif"
+        ></img>
+      </h3>
       <br></br>
-      <h3 className={PillDetailCSS.PillDetailTitle}>알약 이름</h3>
-      <h3 className={PillDetailCSS.CompanyName}>회사 이름</h3>
+      <h3 className={PillDetailCSS.PillDetailTitle}>에이서캡슐(아세클로페낙)</h3>
+      <h3 className={PillDetailCSS.CompanyName}>경동제약(주)</h3>
       <br></br>
-      <Label value={"효능"} content={"효능 내용"}></Label>
-      <Label value={"주의사항"} content={"주의사항 내용"}></Label>
-      <Label value={"복용방법"} content={"복용방법 내용"}></Label>
-      <Label value={"동시 복용 불가 약품"} content={"동시 복용 불가 약품 내용"}></Label>
-      <Label value={"성분표"} content={"성분표 내용"}></Label>
+      <Label value={"효능"} content={"류마티스관절염,강직척추염"}></Label>
+      <Label value={"주의사항"} content={"위장관계 위험"}></Label>
+      <Label value={"복용방법"} content={"이 약은 씹거나 부수지 말고 그대로 복용한다."}></Label>
+      <Label value={"동시 복용 불가 약품"} content={"타이레놀"}></Label>
+      <Label value={"성분표"} content={"아세클로페낙(EP) 100mg"}></Label>
     </>
   );
 }
