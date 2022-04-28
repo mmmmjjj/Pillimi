200155ebcd5cd5fff211f18c00fd8d8961f8ab0b
hyerin <lyyyn@naver.com>
2022-04-28 11:36:10 +0900

    Feat: 약 검색 페이지 화면 만들기 #S06P31A307-124

---
+++ frontend/src/components/pill/PillSearch.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Input, Button, Card } from "reactstrap";
+
+import PillSearchCSS from "./PillSearch.module.css";
+
+function PillSearch() {
+  React.useEffect(() => {}, []);
+  return (
+    <>
+      <br></br>
+      <h3 className={PillSearchCSS.PillDetailTitle}>검색</h3>
+      <div className={PillSearchCSS.Whole}>
+        <hr></hr>
+        <br></br>
+        <Input className={PillSearchCSS.SearchInput} placeholder="검색어를 입력해주세요" type="text"></Input>
+        <Button className={PillSearchCSS.SearchBtn}>검색</Button>
+        <br></br>
+        <Card className={PillSearchCSS.PillList} onClick={gotoPillDetail}>
+          {/* <div className="align-items-center justify-content-left">
+            <div className="align-items-center"> */}
+          <div className="d-flex align-items-center">
+            <img
+              className={`${PillSearchCSS.Img}`}
+              alt="pillImg"
+              src="http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif"
+            ></img>
+            <span className={`${PillSearchCSS.PillName} flex-fill`}>타이레놀정500mg</span>
+            <i className={`now-ui-icons arrows-1_minimal-right`}></i>
+          </div>
+          {/* </div>
+          </div> */}
+        </Card>
+      </div>
+    </>
+  );
+}
+
+function gotoPillDetail() {
+  window.location.href = "/pill-detail";
+}
+
+export default PillSearch;
