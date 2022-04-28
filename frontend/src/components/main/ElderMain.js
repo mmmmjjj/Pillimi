2fcff1da58275e9cc26e7a45f58a2051239bf4c1
hyerin <lyyyn@naver.com>
2022-04-28 13:56:35 +0900

    Refactor: 함수로 반복 코드 수정

--- frontend/src/components/main/ElderMain.js
+++ frontend/src/components/main/ElderMain.js
@@ -1,54 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import ElderMainCSS from "./css/ElderMain.module.css";
 
 function ElderMain() {
   React.useEffect(() => {}, []);
-  const RemoveLine = {
-    textDecoration: "none",
-  };
   return (
     <>
       <div className={ElderMainCSS.Whole}>
         <div className={ElderMainCSS.Header}></div>
         <div className={ElderMainCSS.MainTitle}>
-          <div className={ElderMainCSS.WhiteBox}>
+          <div className={ElderMainCSS.WhiteBox} onClick={gotoPillToday}>
             <div className={ElderMainCSS.WhiteBoxHeight}></div>
             <br></br>
-            <Link to="/pill-today" style={RemoveLine}>
-              <h3 className={ElderMainCSS.MainText}>오늘 먹을 약</h3>
-            </Link>
+            <h3 className={ElderMainCSS.MainText}>오늘 먹을 약</h3>
             <div className={ElderMainCSS.WhiteBoxHeight}></div>
           </div>
-          <br></br>
-          <br></br>
-          <div className={ElderMainCSS.WhiteBox}>
-            <div className={ElderMainCSS.WhiteBoxHeight}></div>
-            <br></br>
-            <h3 className={ElderMainCSS.MainText}>내 약</h3>
-            <div className={ElderMainCSS.WhiteBoxHeight}></div>
-          </div>
-          <br></br>
-          <br></br>
-          <div className={ElderMainCSS.WhiteBox}>
-            <div className={ElderMainCSS.WhiteBoxHeight}></div>
-            <br></br>
-            <h3 className={ElderMainCSS.MainText}>가족</h3>
-            <div className={ElderMainCSS.WhiteBoxHeight}></div>
-          </div>
-          <br></br>
-          <br></br>
-          <div className={ElderMainCSS.WhiteBox}>
-            <div className={ElderMainCSS.WhiteBoxHeight}></div>
-            <br></br>
-            <h3 className={ElderMainCSS.MainText}>내 정보</h3>
-            <div className={ElderMainCSS.WhiteBoxHeight}></div>
-          </div>
+          <Menu content={"내 약"}></Menu>
+          <Menu content={"가족"}></Menu>
+          <Menu content={"내 정보"}></Menu>
         </div>
       </div>
     </>
   );
+}
+
+function Menu(params) {
+  return (
+    <>
+      <br></br>
+      <br></br>
+      <div className={ElderMainCSS.WhiteBox}>
+        <div className={ElderMainCSS.WhiteBoxHeight}></div>
+        <br></br>
+        <h3 className={ElderMainCSS.MainText}>{params.content}</h3>
+        <div className={ElderMainCSS.WhiteBoxHeight}></div>
+      </div>
+    </>
+  );
+}
+
+function gotoPillToday() {
+  window.location.href = "/pill-today";
 }
 
 export default ElderMain;
