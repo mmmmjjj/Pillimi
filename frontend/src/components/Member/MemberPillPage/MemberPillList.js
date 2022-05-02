0bf928bdf4cde2ca89273fee9f7c30af511f5ea9
고주희 <budludl@naver.com>
2022-05-02 15:22:31 +0900

    Refactor: 현재 복용 약과 이전 복용 약 구분 #S06P31A307-143

--- frontend/src/components/Member/MemberPillPage/MemberPillList.js
+++ frontend/src/components/Member/MemberPillPage/MemberPillList.js
@@ -2,10 +2,7 @@
 import React from "react";
 
 // reactstrap components
-import { Container } from "reactstrap";
 import style from "../css/MemberPillCheck.module.css"
-import { AiOutlineCheckCircle } from 'react-icons/ai';
-import MemberPillPage from "../MemberPillPage";
 
 // core components
 
@@ -18,7 +15,8 @@ function MemberPillList(props) {
       routineDate: [0,1,2,3,4,5,6],
       routineNumber: 1,
       routineTime: "19:00",
-      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif"
+      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
+      isNow: true,
     },
     {
       name: "나제론오디정0.1mg",
@@ -26,7 +24,26 @@ function MemberPillList(props) {
       routineDate: [0,3,6],
       routineNumber: 2,
       routineTime: "9:00",
-      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif"
+      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
+      isNow: true,
+    },
+    {
+      name: "타이레놀",
+      alias: "두통약",
+      routineDate: [0,1,4,5,6],
+      routineNumber: 1,
+      routineTime: "19:00",
+      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
+      isNow: false,
+    },
+    {
+      name: "나제론오디정0.1mg",
+      alias: "고혈압약",
+      routineDate: [0,3,4,6],
+      routineNumber: 2,
+      routineTime: "9:00",
+      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
+      isNow: false,
     }
   ];
 
@@ -43,24 +60,26 @@ function MemberPillList(props) {
     }
     return result;
   }
-  const PillList =  () => {
+  const PillList =  (props) => {
     let result = [];
     array1.forEach(element =>{
-      console.log(element.type);
-      result.push(<div className="d-flex align-items-center ">
-        <div className={`${style.imgsize2} ml-2 flex-fill`}>
-          <img src={element.img} className={`${style.size}`}></img>
-        </div>
-        <div className="flex-fill">
-          <span>{element.name}</span><br></br>
-          <span>({element.alias})</span><br></br>
-          <span>
-            <RoutineDate value={element.routineDate}></RoutineDate>
-          </span>&nbsp;/&nbsp;
-          <span>1회 {element.routineNumber}정</span>&nbsp;/&nbsp;
-          <span>{element.routineTime}</span>
-        </div>
-      </div>)
+      if(element.isNow==props.isNow){
+        console.log(element.type);
+        result.push(<div className={`d-flex align-items-center flex-row pl-3 ${style.checkAlarm2} `}>
+          <div className={`${style.imgsize2} ml-2`}>
+            <img src={element.img} className={`${style.size}`}></img>
+          </div>
+          <div className="flex-fill">
+            <span>{element.name}</span><br></br>
+            <span>({element.alias})</span><br></br>
+            <span>
+              <RoutineDate value={element.routineDate}></RoutineDate>
+            </span>&nbsp;/&nbsp;
+            <span>1회 {element.routineNumber}정</span>&nbsp;/&nbsp;
+            <span>{element.routineTime}</span>
+          </div>
+        </div>)
+      }
     })
     return result;
   };
@@ -68,7 +87,22 @@ function MemberPillList(props) {
   return (
     <>  
       <div className={`${style.center} ${style.whole}`}>
-        <PillList></PillList>
+        <div className="d-flex">
+          <div className="flex-fill pt-2 pb-2 pr-4 m-0 ">
+            약
+          </div>
+          <div className="flex-fill pt-2 pb-2 border border-top-0 border-dark bg-white">
+            복용확인
+          </div>
+        </div>
+        <div className="pt-4">
+          <h5>현재 복용 중인 약</h5>
+          <PillList isNow={true}></PillList>
+        </div>
+        <div className="pt-4">
+          <h5>이전에 복용한 약</h5>
+          <PillList isNow={false}></PillList>
+        </div>
       </div>
     </>
   )
