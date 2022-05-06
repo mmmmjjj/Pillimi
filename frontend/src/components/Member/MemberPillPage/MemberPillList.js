d94f0247569571d31fbe75a09a6dcd9ab54cc5cd
고주희 <budludl@naver.com>
2022-05-06 11:56:51 +0900

    Refactor: 목록에서 요일, 시간, 개수 제거

--- frontend/src/components/Member/MemberPillPage/MemberPillList.js
+++ frontend/src/components/Member/MemberPillPage/MemberPillList.js
@@ -7,59 +7,7 @@ import style from "../css/MemberPillCheck.module.css"
 // core components
 
 function MemberPillList(props) {
-
-  const array1 = [
-    {
-      name: "나제론오디정0.1mg",
-      alias: "고혈압약",
-      routineDate: [0,1,2,3,4,5,6],
-      routineNumber: 1,
-      routineTime: "19:00",
-      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
-      isNow: true,
-    },
-    {
-      name: "나제론오디정0.1mg",
-      alias: "고혈압약",
-      routineDate: [0,3,6],
-      routineNumber: 2,
-      routineTime: "9:00",
-      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
-      isNow: true,
-    },
-    {
-      name: "타이레놀",
-      alias: "두통약",
-      routineDate: [0,1,4,5,6],
-      routineNumber: 1,
-      routineTime: "19:00",
-      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
-      isNow: false,
-    },
-    {
-      name: "나제론오디정0.1mg",
-      alias: "고혈압약",
-      routineDate: [0,3,4,6],
-      routineNumber: 2,
-      routineTime: "9:00",
-      img: "http://ticketimage.interpark.com/PlayDictionary/DATA/PlayDic/PlayDicUpload/040001/21/09/0400012109_168370_01.539.gif",
-      isNow: false,
-    }
-  ];
-
-  const RoutineDate = (props) => {
-    let result = []
-    console.log(props)
-    if(props.value.length===7){
-      result.push(<span>매일</span>)
-    }else{
-      let Week = ["월", "화", "수", "목", "금", "토", "일"];
-      props.value.forEach(element=>{
-        result.push(<span>{Week[element]}&nbsp;</span>)
-      })
-    }
-    return result;
-  }
+  
   const PillList =  (props) => {
     let result = [];
     array1.forEach(element =>{
@@ -72,11 +20,6 @@ function MemberPillList(props) {
           <div className="flex-fill">
             <span>{element.name}</span><br></br>
             <span>({element.alias})</span><br></br>
-            <span>
-              <RoutineDate value={element.routineDate}></RoutineDate>
-            </span>&nbsp;/&nbsp;
-            <span>1회 {element.routineNumber}정</span>&nbsp;/&nbsp;
-            <span>{element.routineTime}</span>
           </div>
         </div>)
       }
