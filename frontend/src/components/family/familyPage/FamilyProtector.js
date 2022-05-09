16fb5a0a64bd78ab6b8a9e5333a0b46e3fb79a34
고주희 <budludl@naver.com>
2022-05-09 13:59:43 +0900

    Refactor: 모달창에 경로 추가함 #S06P31A307-186

--- frontend/src/components/family/familyPage/FamilyProtector.js
+++ frontend/src/components/family/familyPage/FamilyProtector.js
@@ -5,9 +5,14 @@ import { getMyFamily } from "api/family"
 
 function FamilyProtector() {
   const [modalbool, setmodalbool] = useState(false)
-  const toggle = () => {
+  const [modalnum, setModalNum] = useState(-1);
+  const toggle = (num) => {
+    console.log(num);
+    setModalNum(num)
     setmodalbool(!modalbool)
   }
+
+  
   
   useEffect(() => {
     console.log("마운트")
@@ -36,7 +41,7 @@ function FamilyProtector() {
     }else{
       familyList.forEach( element => {
         result.push(
-          <ListGroupItem onClick={toggle} key={element.memberSeq}>
+          <ListGroupItem onClick={(e)=>{toggle(element.memberSeq, e)}} key={element.memberSeq}>
             <Row xs="4">
               <Col>
                 <img className="listimg" alt="" src={element.memberImage} />
@@ -52,6 +57,23 @@ function FamilyProtector() {
     return result;
   }
 
+  const ProtegeModal = () => {
+    console.log(modalnum)
+    return(
+      <Modal isOpen={modalbool} toggle={(e) => toggle(-1, e)} centered>
+        <Row style={{ padding:"20px",borderBottom:"1px solid black"}} onClick={gotoMemberInfoDetail}><h3 className="familyh3">회원 정보</h3></Row>
+        <Row style={{ padding:"20px" }}><h3 className="familyh3" onClick={gotoMedicineList}>약 관리</h3></Row>
+      </Modal>
+    )
+  }
+
+  const gotoMemberInfoDetail = () => {
+    window.location.href = `/member-info/member-info-detail/` + modalnum;
+  }
+
+  const gotoMedicineList = () => {
+    window.location.href = `/member-pill-page/member-pill-list/` + modalnum;
+  }
   return (
     <div
       style={{
@@ -69,52 +91,9 @@ function FamilyProtector() {
           paddingTop: "50px",
         }}
       >
-        <Family></Family>
-        {/* <ListGroupItem onClick={toggle}>
-          <Row xs="4">
-            <Col>
-              <img className="listimg" alt="" src="/img/1.jpg" />
-            </Col>
-            <Col xs={{ offset: 1, size: 8 }} className="listitemtext">
-              <h3 className="familyh3">엄마</h3>
-            </Col>
-          </Row>
-        </ListGroupItem>
-        <ListGroupItem>
-          <Row xs="4">
-            <Col>
-              <img className="listimg" alt="" src="/img/2.jpg" />
-            </Col>
-            <Col xs={{ offset: 1, size: 8 }} className="listitemtext">
-              <h3 className="familyh3">아빠</h3>
-            </Col>
-          </Row>
-        </ListGroupItem>
-        <ListGroupItem>
-          <Row xs="4">
-            <Col>
-              <img className="listimg" alt="" src="/img/3.jpg" />
-            </Col>
-            <Col xs={{ offset: 1, size: 8 }} className="listitemtext">
-              <h3 className="familyh3">할머니</h3>
-            </Col>
-          </Row>
-        </ListGroupItem>
-        <ListGroupItem>
-          <Row xs="4">
-            <Col>
-              <img className="listimg" alt="" src="/img/4.jpg" />
-            </Col>
-            <Col xs={{ offset: 1, size: 8 }} className="listitemtext">
-              <h3 className="familyh3">할아버지</h3>
-            </Col>
-          </Row>
-        </ListGroupItem> */}
+      <Family></Family>
       </ListGroup>
-      <Modal isOpen={modalbool} toggle={toggle} centered>
-        <Row style={{ padding:"20px",borderBottom:"1px solid black"}}><h3 className="familyh3">회원 정보</h3></Row>
-        <Row style={{ padding:"20px" }}><h3 className="familyh3">약 관리</h3></Row>
-      </Modal>
+      <ProtegeModal></ProtegeModal>
     </div>
   );
 }
