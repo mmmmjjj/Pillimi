b042a2da08f37e89d4ce0d94bff4d7dafa2d8911
hyerin <lyyyn@naver.com>
2022-05-10 11:39:39 +0900

    Style: 경로에 seq 추가

--- frontend/src/components/pill/PillTake.js
+++ frontend/src/components/pill/PillTake.js
@@ -15,7 +15,11 @@ function PillTake(props) {
           <Switch>
             <Route exact path={`${basicurl}`} render={(props) => <PillTakeRegister {...props} />} />
             <Route exact path={`${basicurl}/modify`} render={(props) => <PillTakeModify {...props} />} />
-            <Route exact path={`${basicurl}/detail`} render={(props) => <PillTakeDetail {...props} />} />
+            <Route
+              exact
+              path={`${basicurl}/detail/:memberMedicineSeq`}
+              render={(props) => <PillTakeDetail {...props} />}
+            />
           </Switch>
         </Switch>
       </BrowserRouter>
