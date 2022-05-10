847055ba5867bc99014518c4da90c9280d272059
hyerin <lyyyn@naver.com>
2022-05-10 21:21:17 +0900

    Fix: 로그인한 상태에서 로그인 화면 건너뛰기

--- frontend/src/components/main/Login.js
+++ frontend/src/components/main/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import kakao_login from "../../assets/img/kakao_login.png";
 // import naver_login from "../../assets/img/naver_login.png";
@@ -7,24 +8,41 @@ import LoginCSS from "./css/Login.module.css";
 import Header from "components/Headers/Header";
 
 function Login() {
-  React.useEffect(() => {}, []);
+  const isLogin = useSelector((state) => state.memberInfo.isLogin);
+  const isProtector = useSelector((state) => state.memberInfo.memberInfo.protector);
+
+  React.useEffect(() => {
+    if (isLogin === true) {
+      if (isProtector === true) {
+        window.location.href = `/pill-today`;
+      } else {
+        window.location.href = `/main`;
+      }
+    }
+  }, [isLogin, isProtector]);
   return (
     <>
-      <Header header="로그인"></Header>
-      <div className={LoginCSS.LoginBtnDiv}>
-        <br></br>
-        <a
-          href="https://kauth.kakao.com/oauth/authorize?client_id=74245badb067a597419134fbf90742dd&redirect_uri=http://localhost:3000/callback
+      {!isLogin ? (
+        <>
+          <Header header="로그인"></Header>
+          <div className={LoginCSS.LoginBtnDiv}>
+            <br></br>
+            <a
+              href="https://kauth.kakao.com/oauth/authorize?client_id=74245badb067a597419134fbf90742dd&redirect_uri=http://localhost:3000/callback
 &response_type=code"
-        >
-          <img src={kakao_login} className={LoginCSS.LoginBtn} alt="kakao_login" />
-        </a>
-        <br></br>
-        <br></br>
-        {/* <Link to="/pill-today">
+            >
+              <img src={kakao_login} className={LoginCSS.LoginBtn} alt="kakao_login" />
+            </a>
+            <br></br>
+            <br></br>
+            {/* <Link to="/pill-today">
           <img src={naver_login} className={LoginCSS.LoginBtn} alt="naver_login" />
         </Link> */}
-      </div>
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 }
