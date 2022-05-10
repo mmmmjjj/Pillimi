6dc4c2a10bd88e354054c78e7e4e8e69f76f22a0
hyerin <lyyyn@naver.com>
2022-05-10 15:17:20 +0900

    Fix: href 방식으로 이동

--- frontend/src/components/Member/MemberPillPage/MemberPillList.js
+++ frontend/src/components/Member/MemberPillPage/MemberPillList.js
@@ -1,72 +1,73 @@
 /*eslint-disable*/
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 // reactstrap components
-import style from "../css/MemberPillCheck.module.css"
-import { getMemberMedicineList } from '../../../api/member'
+import style from "../css/MemberPillCheck.module.css";
+import { getMemberMedicineList } from "../../../api/member";
 
 // core components
 
 function MemberPillList(props) {
-  
   const memberSeq = props.match.params.memberSeq;
 
   const [pills, setPills] = useState([]);
 
   useEffect(() => {
-    console.log("마운트")
+    console.log("마운트");
     // console.log(match.params.memberSeq);
     getMediList();
-  },[])
+  }, []);
 
   const getMediList = () => {
-    getMemberMedicineList(memberSeq,
-      ( success ) => {
-        console.log(success)
-        setPills(success.data.data)
-      }, ( fail ) => {
-        console.log(fail)
-      })
-  }
-
-  const gotoMedicineDetail = (element) => {
-    props.history.push({
-      pathname: `/pill-take/detail/${element.memberMedicineSeq}`,
-      props: {
-        mediName: element.medicineName
+    getMemberMedicineList(
+      memberSeq,
+      (success) => {
+        console.log(success);
+        setPills(success.data.data);
+      },
+      (fail) => {
+        console.log(fail);
       }
-    })
-  }
+    );
+  };
 
-  const PillList =  (props) => {
+  const gotoMedicineDetail = (memMediSeq) => {
+    window.location.href = "/pill-take/detail/" + memMediSeq;
+  };
+
+  const PillList = (props) => {
     let result = [];
-        console.log(pills)
-    pills.forEach(element =>{
-      if(element.now==props.isNow){
-        result.push(<div className={`d-flex align-items-center flex-row pl-3 pr-2 ${style.checkAlarm2} `} onClick={() => gotoMedicineDetail(element)}> 
-          <div className={`${style.imgsize2} ml-2`}>
-            <img src={element.imageURL} className={`${style.size}`}></img>
+    console.log(pills);
+    pills.forEach((element) => {
+      if (element.now == props.isNow) {
+        result.push(
+          <div
+            className={`d-flex align-items-center flex-row pl-3 pr-2 ${style.checkAlarm2} `}
+            onClick={() => gotoMedicineDetail(element.memberMedicineSeq)}
+          >
+            <div className={`${style.imgsize2} ml-2`}>
+              <img src={element.imageURL} className={`${style.size}`}></img>
+            </div>
+            <div className="flex-fill">
+              <span>{element.medicineName}</span>
+              <br></br>
+              <span>({element.memberMedicineName})</span>
+              <br></br>
+            </div>
           </div>
-          <div className="flex-fill">
-            <span>{element.medicineName}</span><br></br>
-            <span>({element.memberMedicineName})</span><br></br>
-          </div>
-        </div>)
+        );
       }
-    })
+    });
     return result;
   };
 
   return (
-    <>  
+    <>
       <div className={`${style.center} ${style.whole}`}>
         <div className="d-flex">
-          <div className="flex-fill pt-2 pb-2 pr-4 m-0 ">
-            약
-          </div>
-          <div className="flex-fill pt-2 pb-2 border border-top-0 border-dark bg-white">
-            복용확인
-          </div>
+          <div className="flex-fill pt-2 pb-2 pr-4 m-0 ">약</div>
+          <div className="flex-fill pt-2 pb-2 border border-top-0 border-dark bg-white">복용확인</div>
         </div>
         <div className="pt-4">
           <h5>현재 복용 중인 약</h5>
@@ -78,8 +79,7 @@ function MemberPillList(props) {
         </div>
       </div>
     </>
-  )
+  );
 }
-
 
 export default MemberPillList;
