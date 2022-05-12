f015f02a9e40a852c2729bedbe595a3fba6a779b
노태현 <godandme@naver.com>
2022-05-12 11:20:29 +0900

    Fix: navbar

--- frontend/src/layout/Navbar.js
+++ frontend/src/layout/Navbar.js
@@ -27,10 +27,58 @@ function Navbar() {
     window.location.href = `/family/Protector`;
   };
   const movemyinfo = () => {
-    window.location.href = `/member-info-detail/member-info-detail/${memberSeq}`;
+    window.location.href = `/member-info/member-info-detail/${memberSeq}`;
   };
   return (
     <>
+    {isProtector ? (
+        <div
+          style={{
+            backgroundColor: "#F1F1F1",
+            margin: "0px",
+            padding: "0px",
+            bottom: "0",
+            width: "100%",
+            height: "10vh",
+          }}
+        >
+          <Row xs="4">
+            <Col className="navcol" onClick={movepilltoday}>
+              <AiFillHome className="fa-3x" />
+              <h6>홈</h6>
+            </Col>
+            <Col className="navcol" onClick={movepillsearch}>
+              <AiOutlineSearch className="fa-3x" />
+              <h6>약 검색</h6>
+            </Col>
+            <Col className="navcol" onClick={moveprotector}>
+              <HiOutlineUserGroup className="fa-3x" />
+              <h6>가족관리</h6>
+            </Col>
+            <Col className="navcol" onClick={movemyinfo}>
+              <CgUserList className="fa-3x" />
+              <h6>내정보</h6>
+            </Col>
+          </Row>
+        </div>
+      ) : (
+        <div
+          style={{
+            backgroundColor: "#F1F1F1",
+            margin: "0px",
+            padding: "0px",
+            bottom: "0",
+            width: "100%",
+            height: "10vh",
+          }}
+        >
+          <Row xs="1">
+            <Col className="navcol" onClick={movemain}>
+              <h2 style={{ margin: "0px" }}>처음 화면으로</h2>
+            </Col>
+          </Row>
+        </div>
+      )}
       {isProtector ? (
         <div
           style={{
@@ -40,6 +88,7 @@ function Navbar() {
             position: "fixed",
             bottom: "0",
             width: "100%",
+            height: "10vh",
           }}
         >
           <Row xs="4">
@@ -70,6 +119,7 @@ function Navbar() {
             position: "fixed",
             bottom: "0",
             width: "100%",
+            height: "10vh",
           }}
         >
           <Row xs="1">
