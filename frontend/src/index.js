c3b8b550265f43d683ea6b34620bd48fc62f65cf
고주희 <budludl@naver.com>
2022-05-17 02:30:26 +0900

    Revert "Remove: PillPicture 삭제"
    This reverts commit 2e39c6fd944dd499eb3b513b95cce2575df0899b.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -46,6 +46,7 @@ import PillToday from "components/main/PillToday.js";
 import PillDetail from "components/pill/PillDetail.js";
 import PillSearch from "components/pill/PillSearch.js";
 import PillTake from "components/pill/PillTake.js";
+import PillPicture from "components/pill/PillPicture.js";
 import Callback from "components/main/Callback.js";
 import MemberInfo from "components/Member/MemberInfo.js";
 import MemberPillPage from "components/Member/MemberPillPage";
@@ -76,6 +77,7 @@ ReactDOM.render(
             <Route exact path="/pill-detail/:pillSeq" render={(props) => <PillDetail {...props} />} />
             <Route exact path="/pill-search" render={(props) => <PillSearch {...props} />} />
             <Route path="/pill-take" render={(props) => <PillTake {...props} />} />
+            <Route path="/pill-picture" render={(props) => <PillPicture {...props} />} />
             <Route path="/member-info" render={(props) => <MemberInfo {...props} />} />
             <Route path="/member-pill-page" render={(props) => <MemberPillPage {...props} />} />
             <Route path="/member-pill-check" render={(props) => <MemberPillCheck {...props} />} />
