e07965872621cb572707c03bf469f2022ec869ec
hyerin <lyyyn@naver.com>
2022-05-16 11:02:01 +0900

    Fix: 알림 크기 줄이기

--- frontend/src/components/family/familyPage/Cameratest.js
+++ frontend/src/components/family/familyPage/Cameratest.js
@@ -5,7 +5,7 @@ import { Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import { addPillTakePicture } from "api/alarm";
 import Swal from "sweetalert2";
-import { useHistory  } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function Cameratest(props) {
@@ -13,12 +13,12 @@ function Cameratest(props) {
   const [picimg, setImage] = useState();
   const [pic, setPicture] = useState(false);
   const alarmSeq = props.match.params.alarmSeq;
-  
+
   const takepic = () => {
     setPicture(true);
   };
   const retry = () => {
-    console.log(picimg.split(','));
+    console.log(picimg.split(","));
     setPicture(false);
   };
 
@@ -26,55 +26,55 @@ function Cameratest(props) {
 
   const sendImage = (event) => {
     console.log(picimg);
-    let picimgData = picimg.split(',');
+    let picimgData = picimg.split(",");
     let imgInfo = {
       alarmSeq: Number(alarmSeq),
-      image: picimgData[1]
-    }
-    console.log(imgInfo)
-    addPillTakePicture(imgInfo, (success) => {
-      console.log(success)
-      event.preventDefault();
-      Swal.fire({
-        icon: "success",
-        title: "사진이 전송되었습니다.",
-        confirmButtonColor: `#0369a1`,
-      }).then(function () {
-        history.push(`/pill-today`)
-      });
-    }, (fail) => {
-      console.log(fail);
-    })
-  }
+      image: picimgData[1],
+    };
+    console.log(imgInfo);
+    addPillTakePicture(
+      imgInfo,
+      (success) => {
+        console.log(success);
+        event.preventDefault();
+        Swal.fire({
+          icon: "success",
+          text: "사진이 전송되었습니다.",
+          width: "80%",
+          confirmButtonColor: `#0369a1`,
+        }).then(function () {
+          history.push(`/pill-today`);
+        });
+      },
+      (fail) => {
+        console.log(fail);
+      }
+    );
+  };
 
   useEffect(() => {
     props.getheader("사진 찍기");
     props.getnavbar(false);
-  },[]);
+  }, []);
 
   let isProtector = useSelector((state) => state.memberInfo.memberInfo.protector);
-  if(isProtector){
+  if (isProtector) {
     Swal.fire({
       icon: "warning",
       title: "권한이 없는 페이지입니다.",
       confirmButtonColor: `#ff0000`,
     }).then(function () {
-      props.history.push(`/`)
+      props.history.push(`/`);
     });
-    return(
-      <div></div>
-    )
+    return <div></div>;
   }
-  
+
   return (
     <>
       <div style={{ height: "40px" }}></div>
       {pic ? (
         <div style={{ width: "80%", margin: "auto" }}>
-          <img
-            src={picimg}
-            alt="다시찍기를 눌러주세요"
-          />
+          <img src={picimg} alt="다시찍기를 눌러주세요" />
           <br />
           <Button className="activebtn" size="lg" onClick={sendImage}>
             전송하기
@@ -85,7 +85,7 @@ function Cameratest(props) {
         </div>
       ) : (
         <div style={{ width: "80%", margin: "auto" }}>
-          <Camera ref={cam} aspectRatio={2 / 3} facingMode={"environment"}/>
+          <Camera ref={cam} aspectRatio={2 / 3} facingMode={"environment"} />
           <Button
             className="activebtn"
             size="lg"
